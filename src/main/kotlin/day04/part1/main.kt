2b03d0c7f2bf2d197d72d44c64cdd51f0ad7cd89
Valentin <valentin.rogister@gmail.com>
2024-12-04 19:11:11 +0100

    day04 part1

--- src/main/kotlin/day04/part1/main.kt
+++ src/main/kotlin/day04/part1/main.kt
@@ -1,25 +1,29 @@
 package day04.part1
 
-data class Point(val x: Int, val y: Int)
-
-fun Map<Point, Char>.compute(): Int {
-    val xMax = this.keys.maxBy { it.x }.x
-    val yMax = this.keys.maxBy { it.y }.y
-
-    return listOf(
-        (0..yMax).map { y -> (0..xMax).map { this[Point(it, y)] } },
-        (0..xMax).map { x -> (0..yMax).map { this[Point(x, it)] } },
-        (0..xMax).map { x -> (0..yMax).map { this[Point(x + it, it)] } },
-        (0..xMax).map { x -> (0..yMax).map { this[Point(x - it, it)] } }
-    )
-        .flatten()
-        .sumOf { l ->
-            l
-                .windowed(4)
-                .map { it.joinToString("") }
-                .count { "XMAS" in listOf(it, it.reversed()) }
-        }
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
 }
+
+fun Map<Point, Char>.compute() =
+    this
+        .filterValues { it == 'X' }
+        .asIterable()
+        .sumOf { (k, v) ->
+            listOf(
+                Point(0, -1),
+                Point(1, -1),
+                Point(1, 0),
+                Point(1, 1),
+                Point(0, 1),
+                Point(-1, 1),
+                Point(-1, 0),
+                Point(-1, -1),
+            ).count {
+                (0..3).fold(k to "") { acc, _ ->
+                    (acc.first + it) to (acc.second + this.getOrDefault(acc.first, '.'))
+                }.second == "XMAS"
+            }
+        }
 
 fun main() {
     generateSequence(::readLine)
@@ -27,7 +31,6 @@ fun main() {
         .trim()
         .replace("\r", "")
         .split("\n")
-        .map { ".".repeat(it.length) + it + ".".repeat(it.length) }
         .flatMapIndexed { y, line -> line.mapIndexed { x, c -> Point(x, y) to c } }
         .toMap()
         .let { println(it.compute()) }
