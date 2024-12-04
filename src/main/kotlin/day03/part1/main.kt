947d29f6ec05378d72c0235ce7d6ec1ab5ebefd8
Valentin Rogister <valentin.rogister@gmail.com>
2024-12-04 01:51:43 +0100

    renamed files

--- src/main/kotlin/day03/part1/main.kt
+++ src/main/kotlin/day03/part1/main.kt
@@ -71,5 +71,7 @@ fun compute(l: List<Token>): Int {
 fun main() {
     generateSequence(::readLine)
         .joinToString("\n")
+        .trim()
+        .replace("\r", "")
         .let { println(compute(lex(it))) }
 }
