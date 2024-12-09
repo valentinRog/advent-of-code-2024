c42e35046500b9e5233af59e964d5c595e2a71c0
Valentin <valentin.rogister@gmail.com>
2024-12-09 18:54:36 +0100

    mutable list

--- src/main/kotlin/day09/part1/main.kt
+++ src/main/kotlin/day09/part1/main.kt
@@ -1,18 +1,14 @@
 package day09.part1
 
-import java.math.BigInteger
-
-fun List<Int>.compute(): List<Int> {
-    val l = this.toMutableList()
+fun MutableList<Int>.compute() {
     tailrec fun compute(i: Int, j: Int) {
         if (i == j) return
-        if (l[i] != -1) return compute(i + 1, j)
-        if (l[j] == -1) return compute(i, j - 1)
-        l[i] = l[j].also { l[j] = -1 }
+        if (this[i] != -1) return compute(i + 1, j)
+        if (this[j] == -1) return compute(i, j - 1)
+        this[i] = this[j].also { this[j] = -1 }
         compute(i + 1, j - 1)
     }
-    compute(0, l.lastIndex)
-    return l
+    compute(0, this.lastIndex)
 }
 
 fun main() =
@@ -24,7 +20,8 @@ fun main() =
         .fold(emptyList<Int>()) { acc, (i, c) ->
             acc + generateSequence { if (i % 2 == 0) i / 2 else -1 }.take(c.toString().toInt()).toList()
         }
-        .compute()
+        .toMutableList()
+        .also { it.compute() }
         .withIndex()
         .filter { it.value != -1 }
         .sumOf { it.value.toBigInteger() * it.index.toBigInteger() }
--- src/main/kotlin/day09/part2/main.kt
+++ src/main/kotlin/day09/part2/main.kt
@@ -1,28 +1,23 @@
 package day09.part2
 
-import java.math.BigInteger
-
-fun List<Int>.compute(): List<Int> {
-    val l = this.toMutableList()
-
+fun MutableList<Int>.compute() {
     tailrec fun insertBlock(i: Int, block: IntRange) {
         if (i in block) return
-        if (l[i] != -1) return insertBlock(i + 1, block)
-        val space = generateSequence(i) { it + 1 }.takeWhile { l[it] == l[i] }.let { i..it.last() }
+        if (this[i] != -1) return insertBlock(i + 1, block)
+        val space = generateSequence(i) { it + 1 }.takeWhile { this[it] == this[i] }.let { i..it.last() }
         if (space.count() < block.count()) return insertBlock(i + 1, block)
-        for ((ii, jj) in space.zip(block)) l[ii] = l[jj].also { l[jj] = l[ii] }
+        for ((ii, jj) in space.zip(block)) this[ii] = this[jj].also { this[jj] = this[ii] }
     }
 
     tailrec fun compute(j: Int) {
         if (j < 0) return
-        if (l[j] == -1) return compute(j - 1)
-        val block = generateSequence(j) { it - 1 }.takeWhile { it >= 0 && l[it] == l[j] }.last()..j
+        if (this[j] == -1) return compute(j - 1)
+        val block = generateSequence(j) { it - 1 }.takeWhile { it >= 0 && this[it] == this[j] }.last()..j
         insertBlock(0, block)
         compute(block.first - 1)
     }
 
-    compute(l.lastIndex)
-    return l
+    compute(this.lastIndex)
 }
 
 fun main() =
@@ -34,7 +29,8 @@ fun main() =
         .fold(emptyList<Int>()) { acc, (i, c) ->
             acc + generateSequence { if (i % 2 == 0) i / 2 else -1 }.take(c.toString().toInt()).toList()
         }
-        .compute()
+        .toMutableList()
+        .also { it.compute() }
         .withIndex()
         .filter { it.value != -1 }
         .sumOf { it.value.toBigInteger() * it.index.toBigInteger() }
