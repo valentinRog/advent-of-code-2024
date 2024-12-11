fe7f7de300caa00b8da6273f4814bfd42c89c899
Valentin <valentin.rogister@gmail.com>
2024-12-11 15:22:04 +0100

    day11. Again, very ez

--- src/main/kotlin/day11/part1/main.kt
+++ src/main/kotlin/day11/part1/main.kt
@@ -5,16 +5,16 @@ import java.math.BigInteger
 class Solver {
     private val cache = mutableMapOf<Pair<BigInteger, Int>, BigInteger>()
 
-    fun compute(n: BigInteger, depth: Int = 0): BigInteger {
+    fun compute(n: BigInteger, depth: Int): BigInteger {
         if (cache.containsKey(n to depth)) return cache.getValue(n to depth)
-        if (depth == 25) return 1.toBigInteger()
-        if (n == 0.toBigInteger()) return cache.getOrPut(n to depth) { compute(1.toBigInteger(), depth + 1) }
+        if (depth == 0) return 1.toBigInteger()
+        if (n == 0.toBigInteger()) return cache.getOrPut(n to depth) { compute(1.toBigInteger(), depth - 1) }
         if (n.toString().length % 2 == 0) {
             val n1 = n.toString().substring(0, n.toString().length / 2).toBigInteger()
             val n2 = n.toString().substring(n.toString().length / 2).toBigInteger()
-            return cache.getOrPut(n to depth) { compute(n1, depth + 1) + compute(n2, depth + 1) }
+            return cache.getOrPut(n to depth) { compute(n1, depth - 1) + compute(n2, depth - 1) }
         }
-        return cache.getOrPut(n to depth) { compute(n * 2024.toBigInteger(), depth + 1) }
+        return cache.getOrPut(n to depth) { compute(n * 2024.toBigInteger(), depth - 1) }
     }
 }
 
@@ -26,6 +26,6 @@ fun main() {
         .replace("\r", "")
         .split(" ")
         .map { it.toBigInteger() }
-        .sumOf { solver.compute(it) }
+        .sumOf { solver.compute(it, 25) }
         .let(::println)
 }
