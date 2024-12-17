b818c21b31eb4396b8625d77b89a00f4329391a1
Valentin Rogister <valentin.rogister@gmail.com>
2024-12-17 22:46:23 +0100

    day17 so f cool

--- src/main/kotlin/day17/part1/main.kt
+++ src/main/kotlin/day17/part1/main.kt
@@ -1,9 +1,12 @@
 package day17.part1
 
-fun Int.pow(n: Int) = generateSequence(this) { it * this }.take(n).last()
+fun Int.pow(n: Int): Int = if (n == 0) 1 else this * pow(n - 1)
 
-data class Computer(var a: Int, var b: Int, var c: Int) {
-    private fun getCombo(n: Int) = when (n) {
+fun List<Int>.compute(a0: Int): List<Int> {
+    var a = a0
+    var b = 0
+    var c = 0
+    fun getCombo(n: Int) = when (n) {
         in 0..3 -> n
         4 -> a
         5 -> b
@@ -11,29 +14,27 @@ data class Computer(var a: Int, var b: Int, var c: Int) {
         else -> throw IllegalArgumentException()
     }
 
-    fun compute(l: List<Int>): List<Int> {
-        val output = mutableListOf<Int>()
-        var i = 0
-        while (i < l.lastIndex) {
-            when (l[i]) {
-                0 -> a /= 2.pow(getCombo(l[i + 1]))
-                1 -> b = b xor l[i + 1]
-                2 -> b = getCombo(l[i + 1]) % 8
-                3 -> if (a != 0) {
-                    i = l[i + 1]
-                    continue
-                }
-
-                4 -> b = b xor c
-                5 -> output.add(getCombo(l[i + 1]).toInt() % 8)
-                6 -> b = a / 2.pow(getCombo(l[i + 1]))
-                7 -> c = a / 2.pow(getCombo(l[i + 1]))
-                else -> throw IllegalArgumentException()
+    val output = mutableListOf<Int>()
+    var i = 0
+    while (i < this.lastIndex) {
+        when (this[i]) {
+            0 -> a /= 2.pow(getCombo(this[i + 1]))
+            1 -> b = b xor this[i + 1]
+            2 -> b = getCombo(this[i + 1]) % 8
+            3 -> if (a != 0) {
+                i = this[i + 1]
+                continue
             }
-            i += 2
+
+            4 -> b = b xor c
+            5 -> output.add(getCombo(this[i + 1]) % 8)
+            6 -> b = a / 2.pow(getCombo(this[i + 1]))
+            7 -> c = a / 2.pow(getCombo(this[i + 1]))
+            else -> throw IllegalArgumentException()
         }
-        return output
+        i += 2
     }
+    return output
 }
 
 fun main() {
@@ -42,12 +43,20 @@ fun main() {
         .trim()
         .replace("\r", "")
 
-    raw
+    val a0 = raw
         .split("\n\n")[0]
         .lines()
-        .map { it.split(" ").last().toInt() }
-        .let { Computer(it[0], it[1], it[2]) }
-        .compute(raw.split("\n\n")[1].split(" ").last().split(",").map { it.toInt() })
-        .joinToString(",")
+        .first()
+        .split(" ")
+        .last()
+        .toInt()
+
+    raw
+        .split("\n\n")[1]
+        .split(" ")
+        .last()
+        .split(",")
+        .map { it.toInt() }
+        .compute(a0)
         .let(::println)
 }
---
+++ src/main/kotlin/day17/part2/main.kt
@@ -0,0 +1,74 @@
+package day17.part2
+
+import kotlin.math.min
+
+fun Long.pow(n: Long): Long = if (n == 0L) 1L else this * pow(n - 1)
+
+fun List<Int>.simulate(a0: Long): List<Int> {
+    var a = a0
+    var b = 0L
+    var c = 0L
+    fun getCombo(n: Int) = when (n) {
+        in 0..3 -> n.toLong()
+        4 -> a
+        5 -> b
+        6 -> c
+        else -> throw IllegalArgumentException()
+    }
+
+    val output = mutableListOf<Int>()
+    var i = 0
+    while (i < this.lastIndex) {
+        when (this[i]) {
+            0 -> a /= 2L.pow(getCombo(this[i + 1]))
+            1 -> b = b xor this[i + 1].toLong()
+            2 -> b = getCombo(this[i + 1]) % 8
+            3 -> if (a != 0L) {
+                i = this[i + 1]
+                continue
+            }
+
+            4 -> b = b xor c
+            5 -> output.add((getCombo(this[i + 1]) % 8).toInt())
+            6 -> b = a / 2L.pow(getCombo(this[i + 1]))
+            7 -> c = a / 2L.pow(getCombo(this[i + 1]))
+            else -> throw IllegalArgumentException()
+        }
+        i += 2
+    }
+    return output
+}
+
+fun List<Int>.compute(): Long {
+    fun backtracking(a: Long, k: Int): Long? {
+        val l = this.simulate(a)
+        if (l == this) {
+            println(l)
+            println(a)
+            return a
+        }
+        if (l.size > this.size || l.subList(0, k) != this.subList(0, k) || k > 15) return null
+        if (l.subList(0, min(k, l.lastIndex)) != this.subList(0, k)) return null
+        for (n1 in 0L..<(1 shl 3)) {
+            for (n2 in 0L..<(1 shl 3)) {
+                val res = backtracking(a or (n1 shl (k * 3) or (n2 shl (k * 3 + (n1 xor 1).toInt()))), k + 1)
+                if (res != null) return res
+            }
+        }
+        return null
+    }
+    return backtracking(0, 0)!!
+}
+
+fun main() =
+    generateSequence(::readLine)
+        .joinToString("\n")
+        .trim()
+        .replace("\r", "")
+        .split("\n\n")[1]
+        .split(" ")
+        .last()
+        .split(",")
+        .map { it.toInt() }
+        .compute()
+        .let(::println)
