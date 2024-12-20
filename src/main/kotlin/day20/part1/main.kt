59f316f962b96f8e9620f8de2de39ace22fbb641
Valentin Rogister <valentin.rogister@gmail.com>
2024-12-20 16:28:56 +0100

    clean day20 part1

--- src/main/kotlin/day20/part1/main.kt
+++ src/main/kotlin/day20/part1/main.kt
@@ -1,7 +1,5 @@
 package day20.part1
 
-import java.util.PriorityQueue
-
 data class Complex(var x: Int, var y: Int) {
     operator fun plus(other: Complex) = Complex(x + other.x, y + other.y)
 
@@ -17,43 +15,37 @@ data class Complex(var x: Int, var y: Int) {
 
 fun Map<Complex, Char>.getKeyByValue(c: Char) = this.asIterable().first { it.value == c }.key
 
-fun Map<Complex, Char>.cheats() =
-    this
-        .filter { it.value in listOf('.', 'E', 'S') }
-        .keys
-        .flatMap { z ->
-            Complex.DIRS.filter { this[z + it] == '#' && this[z + it + it] in listOf('.', 'E', 'S') }
-                .map { z to z + it + it }
-        }
-
-fun Map<Complex, Char>.dijkstra(): Int {
-
-    fun dijkstra(cheat: Pair<Complex, Complex>?): Int {
-        data class Node(val z: Complex, val cost: Int)
-
-        val m = mutableMapOf<Complex, Int>()
-        val q = PriorityQueue<Node> { e1, e2 -> e1.cost.compareTo(e2.cost) }
-        q.add(Node(this.getKeyByValue('S'), 0))
-        val z1 = this.getKeyByValue('E')
-        while (true) {
-            val (z, cost) = q.poll()!!
-            m[z] = cost
-            if (z == z1) return cost
-            if (cheat != null && z == cheat.first && cheat.second !in m) {
-                q.add(Node(cheat.second, cost + 2))
-            } else {
-                Complex.DIRS.map { z + it }.filter { this[it] != '#' && it !in m }
-                    .forEach { q.add(Node(it, cost + 1)) }
-            }
-        }
-
+fun Map<Complex, Char>.makeDistanceMatrix(): Map<Complex, Int> {
+    val m = mutableMapOf<Complex, Int>()
+    tailrec fun f(z: Complex, zp: Complex?, n: Int) {
+        m[z] = n
+        if (this[z] == 'S') return
+        val d = Complex.DIRS.first { this[z + it] != '#' && z + it != zp }
+        return f(z + d, z, n + 1)
     }
-
-    val n = dijkstra(null)
-    return this.cheats().count { n - dijkstra(it) >= 100 }
+    f(this.getKeyByValue('E'), null, 0)
+    return m
 }
 
-fun main() {
+fun Map<Complex, Char>.compute(): Int {
+    val m = this.makeDistanceMatrix()
+    val cheats = mutableMapOf<Pair<Complex, Complex>, Int>()
+    tailrec fun dfs(z: Complex, zp: Complex?, cost: Int) {
+        if (this[z] == 'E') return
+        val d = Complex.DIRS.first { this[z + it] != '#' && z + it != zp }
+        Complex
+            .DIRS
+            .filter { this[z + it] == '#' && this[z + it + it] in ".ES".toList() }
+            .map { z + it + it }
+            .forEach { cheats[z to it] = m.getValue(it) + cost + 2 }
+        dfs(z + d, z, cost + 1)
+    }
+    dfs(this.getKeyByValue('S'), null, 0)
+    val n = m.getValue(this.getKeyByValue('S'))
+    return cheats.values.count { n - it >= 100 }
+}
+
+fun main() =
     generateSequence(::readLine)
         .joinToString("\n")
         .trim()
@@ -61,6 +53,5 @@ fun main() {
         .lines()
         .flatMapIndexed { y, line -> line.mapIndexed { x, c -> Complex(x, y) to c } }
         .toMap()
-        .dijkstra()
+        .compute()
         .let(::println)
-}
