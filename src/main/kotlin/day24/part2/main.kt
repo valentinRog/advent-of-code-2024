4684309fc2fb9f51740c5693ad07ed0dcf1d2cca
Valentin Rogister <valentin.rogister@gmail.com>
2024-12-28 19:25:27 +0100

    Kt is f cool

--- src/main/kotlin/day24/part2/main.kt
+++ src/main/kotlin/day24/part2/main.kt
@@ -55,13 +55,15 @@ fun List<List<String>>.compute(): String {
     return backtracking(0, emptyMap())!!
 }
 
-val mask = (0..<zMax).fold(0L) { acc, i -> acc or ((i % 2).toLong() shl i) }
-fun List<List<String>>.check(nBits: Int, swaps: Map<String, String>): Boolean {
-    val n = (1L shl nBits) - 1
-    return listOf(n, n and mask, n and (mask.inv()))
-        .asSequence()
-        .combinations()
-        .all { (n1, n2) -> this.simulate(n1, n2, swaps) == n1 + n2 }
+val check: List<List<String>>.(nBits: Int, swaps: Map<String, String>) -> Boolean = run {
+    val mask = (0..<zMax).fold(0L) { acc, i -> acc or ((i % 2).toLong() shl i) }
+    (fun List<List<String>>.(nBits: Int, swaps: Map<String, String>): Boolean {
+        val n = (1L shl nBits) - 1
+        return listOf(n, n and mask, n and (mask.inv()))
+            .asSequence()
+            .combinations()
+            .all { (n1, n2) -> this.simulate(n1, n2, swaps) == n1 + n2 }
+    })
 }
 
 fun main() =
