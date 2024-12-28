91658c9ba98fc5fc4fbe634c64c5b32e90852126
Valentin Rogister <valentin.rogister@gmail.com>
2024-12-28 07:13:42 +0100

    Saved Christmas for the 8th time

---
+++ src/main/kotlin/day25/part1/main.kt
@@ -0,0 +1,20 @@
+package day25.part1
+
+data class Complex(var x: Int, var y: Int)
+
+fun <T> Sequence<T>.combinations() = this.flatMapIndexed { i, e1 -> this.drop(i + 1).map { e1 to it } }
+
+fun Pair<Map<Complex, Char>, Map<Complex, Char>>.fit() =
+    this.first.asSequence().find { (k, v) -> v == '#' && this.second[k] == '#' } == null
+
+fun main() =
+    generateSequence(::readLine)
+        .joinToString("\n")
+        .trim()
+        .replace("\r", "")
+        .split("\n\n")
+        .map { it.lines().flatMapIndexed { y, line -> line.mapIndexed { x, c -> Complex(x, y) to c } }.toMap() }
+        .asSequence()
+        .combinations()
+        .count { it.fit() }
+        .let(::println)
